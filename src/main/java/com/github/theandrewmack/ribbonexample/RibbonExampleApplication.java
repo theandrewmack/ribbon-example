822290d17bbe813b1dee377a0e29cd4473d46bda
Andrew Mack <amack1221@gmail.com>
2019-01-25 21:34:27 -0600

    initial commit

---
+++ src/main/java/com/github/theandrewmack/ribbonexample/RibbonExampleApplication.java
@@ -0,0 +1,14 @@
+package com.github.theandrewmack.ribbonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonExampleApplication.class, args);
+	}
+
+}
+
